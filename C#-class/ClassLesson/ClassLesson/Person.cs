b63402e012f3f62109e5728e89e75fba69b507f8
dominik.kasprzak <dominik.kasprzak@futurum.tech>
2022-07-30 16:22:51 +0200

    Classes

---
+++ C#-class/ClassLesson/ClassLesson/Person.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLesson
+{
+    class Person
+    {
+        public string Name;
+        public string Surnme;
+        private DateTime dateOfBirth;
+        private string contactNumber;
+
+        public static int Count = 0;
+
+        public string ContactNumber
+        {
+            get { return contactNumber; }
+            set 
+            {   if (value.Length < 9)
+                {
+                    Console.WriteLine("Invalid contact number");
+                }
+                else
+                {
+                    contactNumber = value;
+                }
+            }
+        }
+
+        public Person(string firstName, string lastName)
+        {
+            Name = firstName;
+            Surnme = lastName;
+            Count++;
+        }
+
+        public Person(DateTime dateOfBirth, string firstName, string lastName) : this(firstName, lastName)
+        {
+            SetDateOfBirth(dateOfBirth);
+        }
+
+        public void SetDateOfBirth(DateTime date)
+        {
+            if (date > DateTime.Now)
+            {
+                Console.WriteLine("Invalid date of birth");
+            }
+            else
+            { 
+                dateOfBirth = date;
+            }
+        }
+
+        public DateTime GetDateOfBirth() => dateOfBirth;
+
+        public void SayHi()
+        {
+            Console.WriteLine($"Hi, I'm {Name}, {GetDateOfBirth()}");
+        }
+    }
+}
---
+++ C#-class/ClassLesson/ClassLesson/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ClassLesson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person bill = new Person("Bill", "Wick");
+
+            bill.SetDateOfBirth(new DateTime(1990, 1, 1));
+
+            Console.WriteLine($"{bill.Name} {bill.GetDateOfBirth()}");
+
+            bill.SayHi();
+            bill.ContactNumber = "999888777";
+            Console.WriteLine(bill.ContactNumber);
+
+            Person john = new Person(new DateTime(1990, 1, 1), "John", "Wick");
+
+            john.SayHi();
+
+            Console.WriteLine(Person.Count);
+        }
+    }
+}
