555e8d18b56529978a0cd5a1bf4b543a14144a63
dominik.kasprzak <dominik.kasprzak@futurum.tech>
2022-07-30 15:38:43 +0200

    This program valid an email

---
+++ C#-regex/Regex/Regex/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace RegexProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Regex regex = new Regex(@"^[a-z0-9]+\.?[a-z0-9]+@[a-z]+\.[a-z]{2,3}$");
+
+            string email = "example.mail12@mail.com";
+
+            bool isMatch = regex.IsMatch(email);
+
+            Console.WriteLine(isMatch);
+        }
+    }
+}
