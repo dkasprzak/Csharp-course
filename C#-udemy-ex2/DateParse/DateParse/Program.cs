0aecdb043da21d906e33824549bfb54581b31941
dominik.kasprzak <dominik.kasprzak@futurum.tech>
2022-07-30 14:55:04 +0200

    The program count days between Date of birth and Today date

---
+++ C#-udemy-ex2/DateParse/DateParse/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace DateParse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Insert the dte of birth (dd.MM.YYYY)");
+
+            string dateOfBirthString = Console.ReadLine();
+
+            DateTime dateOfBirth = DateTime.Parse(dateOfBirthString);
+
+            TimeSpan timeSpan = DateTime.Now - dateOfBirth;
+            
+
+            Console.WriteLine($"You were born {timeSpan.TotalDays} days ago");
+        }
+    }
+}
